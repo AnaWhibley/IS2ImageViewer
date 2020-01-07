26f189fac1dd646e7363e1bf3df490f37cddca8e
AnaWhibley <anawhibley@gmail.com>
2020-01-07 11:08:02 +0000

    First version working

--- src/imageviewer/presenter/Main.java
+++ src/imageviewer/presenter/Main.java
@@ -3,5 +3,7 @@ package imageviewer.presenter;
 public class Main {
     public static void main(String[] args) {
         System.out.println("IS2 - Image Viewer working...");
+        FileImageLoader imageLoader = new FileImageLoader("images");
+        new MainFrame(imageLoader);
     }
 }
